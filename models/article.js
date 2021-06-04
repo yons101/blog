f93443001d612c8d58627938ceddd519888ca84c
yons101 <wakhmajja@gmail.com>
2021-06-04 01:11:34 +0100

    added article  model & migration

---
+++ models/article.js
@@ -0,0 +1,27 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Article extends Model {
+    static associate(models) {
+      Article.belongsTo(models.User);
+      Article.hasMany(models.Comment);
+      Article.belongsToMany(models.Tag, { through: "ArticleTags" });
+    }
+  }
+  Article.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      content: DataTypes.TEXT,
+      published: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Article",
+    }
+  );
+  return Article;
+};
