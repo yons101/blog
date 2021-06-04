7ac273d56f0c0ed2c7c4a1de835833a747d26117
yons101 <wakhmajja@gmail.com>
2021-06-04 01:25:22 +0100

    added tag routes

---
+++ routes/tags.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+const tagsRepo = require("../repositories/tags");
+
+// GET all tags.
+router.get("/", async function (req, res, next) {
+  res.json(await tagsRepo.getAllTags());
+});
+
+// GET tags by offset and limit
+router.get("/:offset/:limit", async function (req, res, next) {
+  res.json(
+    await tagsRepo.getTags(
+      parseInt(req.params.offset),
+      parseInt(req.params.limit)
+    )
+  );
+});
+// GET tag with an id
+router.get("/:id", async function (req, res, next) {
+  res.json(await tagsRepo.getTag(req.params.id));
+});
+// Add a tag
+router.post("/", async function (req, res, next) {
+  res.json(await tagsRepo.addTag(req.body));
+});
+// Update tag with id
+router.put("/:id", async function (req, res, next) {
+  const tag = await tagsRepo.getTag(req.params.id);
+  if (tag.length == 0) {
+    res.json({ error: `No tag with id ${req.params.id}!` });
+  }
+  res.json(await tagsRepo.updateTag(req.body, req.params.id));
+});
+// Delete tag with id
+router.delete("/:id", async function (req, res, next) {
+  const tags = await tagsRepo.getAllTags();
+  let isFound = false;
+  tags.forEach((tag) => {
+    if (tag.id == req.params.id) {
+      isFound = true;
+      return;
+    }
+  });
+  if (isFound) {
+    tagsRepo.deleteTag(parseInt(req.params.id));
+    res.json({ message: `Tag with id ${req.params.id} has been deleted!` });
+  }
+  res.json({ error: `No tag with id ${req.params.id}!` });
+});
+
+module.exports = router;
