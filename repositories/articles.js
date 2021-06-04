47e511d19b45b9f81dd40cfe393e5f02d85549f9
yons101 <wakhmajja@gmail.com>
2021-06-04 01:19:45 +0100

    added article repository

---
+++ repositories/articles.js
@@ -0,0 +1,41 @@
+const { Article } = require("../models");
+module.exports = {
+  getAllArticles() {
+    return Article.findAll();
+  },
+  test(article, tag) {
+    return article.addTags(tag);
+  },
+  getArticles(offset, limit) {
+    return Article.findAll({ offset, limit });
+  },
+  getArticle(id) {
+    return Article.findAll({
+      where: {
+        id,
+      },
+    });
+  },
+  addArticle(article) {
+    return Article.create(article);
+  },
+  async updateArticle(article, id) {
+    await Article.update(article, {
+      where: {
+        id,
+      },
+    });
+    return Article.findAll({
+      where: {
+        id,
+      },
+    });
+  },
+  deleteArticle(id) {
+    Article.destroy({
+      where: {
+        id,
+      },
+    });
+  },
+};
