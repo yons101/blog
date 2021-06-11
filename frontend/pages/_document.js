45c0ebe502664987e880d576552d91b414b78f7f
yons101 <wakhmajja@gmail.com>
2021-06-11 23:49:33 +0100

    bootsrap theme

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -11,10 +11,8 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+            href="https://bootswatch.com/5/lumen/bootstrap.min.css"
             rel="stylesheet"
-            integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
-            crossOrigin="anonymous"
           />
         </Head>
         <body>
