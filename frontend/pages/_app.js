bb0c62e8bc2f13d0ac110aefbf8d10f4b3fcbbd4
yons101 <wakhmajja@gmail.com>
2021-06-16 18:15:00 +0100

    added a simple footer

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <footer className="bg-secondary text-center p-4 border-top mt-5">
+        Blog101 © 2021
+      </footer>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
