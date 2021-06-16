6eeb26924e4b78bda529ef8e588c320ceb0a963a
yons101 <wakhmajja@gmail.com>
2021-06-16 18:21:56 +0100

    move footer to it's own component

---
+++ frontend/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-secondary text-center p-4 border-top mt-5">
+      Blog101 © 2021
+    </footer>
+  );
+};
+export default Footer;
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,13 +1,12 @@
 import "../styles/globals.css";
 import Header from "@components/Header";
+import Footer from "@components/Footer";
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
-      <footer className="bg-secondary text-center p-4 border-top mt-5">
-        Blog101 © 2021
-      </footer>
+      <Footer />
     </>
   );
 }
